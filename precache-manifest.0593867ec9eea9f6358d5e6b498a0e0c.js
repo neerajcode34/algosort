af7b1c0dce9a8ecf8491486743d4d2621dca5a76
neeraj1434 <neerajkumarm345@gmail.com>
2022-11-11 00:21:28 +0530

    Updates

---
+++ precache-manifest.0593867ec9eea9f6358d5e6b498a0e0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98bf5587e620a4028b0940c48c2946e5",
+    "url": "/algosort/index.html"
+  },
+  {
+    "revision": "17c73a96adb3409e514d",
+    "url": "/algosort/static/css/main.8c5c810e.chunk.css"
+  },
+  {
+    "revision": "e88ae9b6a65fe1c8aa11",
+    "url": "/algosort/static/js/2.fc7f14af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/algosort/static/js/2.fc7f14af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17c73a96adb3409e514d",
+    "url": "/algosort/static/js/main.ed5e9bb2.chunk.js"
+  },
+  {
+    "revision": "ef81628e0c014268b9cf",
+    "url": "/algosort/static/js/runtime-main.ca4cef01.js"
+  }
+]);
